bb6c72d857e9c956bf6d86f09eee2c7389923143
Hajlaouiloujain <loujain.hajlaoui@esprit.tn>
2024-03-26 16:42:04 +0100

    Actor crud + uncompleted front

--- src/Controller/ActorController.php
+++ src/Controller/ActorController.php
@@ -17,12 +17,20 @@ class ActorController extends AbstractController
     #[Route('/', name: 'app_actor_index', methods: ['GET'])]
     public function index(ActorRepository $actorRepository): Response
     {
-        return $this->render('actor/index.html.twig', [
+        flash()->addSuccess("successfully added actor");
+        $form = $this->createForm(ActorType::class, new Actor());
+        $updateForms = array();
+        for ($i = 0; $i < count($actorRepository->findAll()); $i++) {
+            $updateForms[$i] = $this->createForm(ActorType::class, $actorRepository->findAll()[$i])->createView();
+        }
+        return $this->render('back/actorTables.html.twig', [
             'actors' => $actorRepository->findAll(),
+            'form' => $form->createView(),
+            'updateForms' => $updateForms,
         ]);
     }
 
-    #[Route('/new', name: 'app_actor_new', methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'app_actor_new', methods: [ 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $actor = new Actor();
@@ -30,6 +38,16 @@ class ActorController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $file = $form['image']->getData();
+
+            $extension = $file->guessExtension();
+            if (!$extension) {
+                // extension cannot be guessed
+                $extension = 'bin';
+            }
+            $filename = rand(1, 99999) . '.' . $extension;
+            $file->move($this->getParameter('kernel.project_dir')."/public/img/actors", $filename);
+            $actor->setImage("/img/actors/" . $filename);
             $entityManager->persist($actor);
             $entityManager->flush();
 
@@ -50,13 +68,23 @@ class ActorController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_actor_edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}/edit', name: 'app_actor_edit', methods: [ 'POST'])]
     public function edit(Request $request, Actor $actor, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(ActorType::class, $actor);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $file = $form['image']->getData();
+
+            $extension = $file->guessExtension();
+            if (!$extension) {
+                // extension cannot be guessed
+                $extension = 'bin';
+            }
+            $filename = rand(1, 99999) . '.' . $extension;
+            $file->move($this->getParameter('kernel.project_dir')."/public/img/actors", $filename);
+            $actor->setImage("/img/actors/" . $filename);
             $entityManager->flush();
 
             return $this->redirectToRoute('app_actor_index', [], Response::HTTP_SEE_OTHER);
