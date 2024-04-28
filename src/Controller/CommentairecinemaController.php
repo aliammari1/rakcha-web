03087b766ec29fcf073aab6bf8197ce477ebb706
Ghaya18 <159494602+Ghaya18@users.noreply.github.com>
2024-04-28 15:38:44 +0200

    ajout commentaire + analyse commentaire

--- src/Controller/CommentairecinemaController.php
+++ src/Controller/CommentairecinemaController.php
@@ -5,77 +5,97 @@ namespace App\Controller;
 use App\Entity\Commentairecinema;
 use App\Form\CommentairecinemaType;
 use App\Repository\CommentairecinemaRepository;
+use App\Repository\UsersRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Cinema;
+use Sentiment\Analyzer;
+
+
 
 #[Route('/commentairecinema')]
 class CommentairecinemaController extends AbstractController
 {
-    #[Route('/', name: 'app_commentairecinema_index', methods: ['GET'])]
-    public function index(CommentairecinemaRepository $commentairecinemaRepository): Response
-    {
-        return $this->render('commentairecinema/index.html.twig', [
-            'commentairecinemas' => $commentairecinemaRepository->findAll(),
-        ]);
-    }
-
-    #[Route('/new', name: 'app_commentairecinema_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    #[Route('/{idCinema}', name: 'app_commentairecinema_index', methods: ['GET'])]
+    public function index(int $idCinema, CommentairecinemaRepository $commentairecinemaRepository, EntityManagerInterface $entityManager): Response
     {
         $commentairecinema = new Commentairecinema();
         $form = $this->createForm(CommentairecinemaType::class, $commentairecinema);
-        $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($commentairecinema);
-            $entityManager->flush();
+        // Récupérer le cinema
+        $cinema = $entityManager->getRepository(Cinema::class)->findOneBy(['idCinema' => $idCinema]);
 
-            return $this->redirectToRoute('app_commentairecinema_index', [], Response::HTTP_SEE_OTHER);
+        return $this->render('cinema/commentCinema.html.twig', [
+            'commentairecinemas' => $commentairecinemaRepository->findAll(), 
+            'form' => $form->createView(),
+            'cinema' => $cinema, // Assurez-vous de passer le cinema au modèle
+        ]);
+    }
+
+    #[Route('/new/{idCinema}', name: 'app_commentairecinema_new', methods: ['GET', 'POST'])]
+    public function new(int $idCinema, Request $request, EntityManagerInterface $entityManager, UsersRepository $usersRepository, CommentairecinemaRepository $commentairecinemaRepository): Response
+    {
+        $cinema = $entityManager->find(Cinema::class, $idCinema);
+        if (!$cinema) {
+            throw $this->createNotFoundException('No cinema found for id ' . $idCinema);
         }
-
-        return $this->renderForm('commentairecinema/new.html.twig', [
-            'commentairecinema' => $commentairecinema,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'app_commentairecinema_show', methods: ['GET'])]
-    public function show(Commentairecinema $commentairecinema): Response
-    {
-        return $this->render('commentairecinema/show.html.twig', [
-            'commentairecinema' => $commentairecinema,
-        ]);
-    }
-
-    #[Route('/{id}/edit', name: 'app_commentairecinema_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Commentairecinema $commentairecinema, EntityManagerInterface $entityManager): Response
-    {
+    
+        $commentairecinema = new Commentairecinema();
         $form = $this->createForm(CommentairecinemaType::class, $commentairecinema);
         $form->handleRequest($request);
-
+    
         if ($form->isSubmitted() && $form->isValid()) {
+            // Traiter le formulaire soumis
+            $commentairecinema->setIdCinema($cinema->getIdCinema()); // Ajouter l'objet Cinema au commentaire
+            $userId = 147;
+    
+            // Analyse de sentiment avec php-sentiment-analyzer
+            $commentText = $commentairecinema->getCommentaire(); // Supposons que vous avez une méthode getComment() dans votre entité Commentairecinema
+            $sentiment = $this->analyseSentiment($commentText);
+    
+            // Le sentiment est retourné sous forme de chaîne ('positive', 'negative', 'neutral')
+            // Vous pouvez utiliser directement cette valeur comme sentiment du commentaire
+            $commentairecinema->setSentiment($sentiment);
+    
+            $user = $usersRepository->find($userId);
+            if ($user->getRole() == 'client') {
+                $commentairecinema->setIdclient($user->getId());
+            }
+            $entityManager->persist($commentairecinema);
             $entityManager->flush();
-
-            return $this->redirectToRoute('app_commentairecinema_index', [], Response::HTTP_SEE_OTHER);
+    
+            return $this->redirectToRoute('app_commentairecinema_index', ['idCinema' => $idCinema], Response::HTTP_SEE_OTHER);
         }
-
-        return $this->renderForm('commentairecinema/edit.html.twig', [
-            'commentairecinema' => $commentairecinema,
-            'form' => $form,
+    
+        return $this->render('cinema/commentCinema.html.twig', [
+            'cinema' => $cinema,
+            'form' => $form->createView(),
         ]);
     }
 
-    #[Route('/{id}', name: 'app_commentairecinema_delete', methods: ['POST'])]
-    public function delete(Request $request, Commentairecinema $commentairecinema, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$commentairecinema->getId(), $request->request->get('_token'))) {
-            $entityManager->remove($commentairecinema);
-            $entityManager->flush();
-        }
+    
+private function analyseSentiment(string $comment): string
+{
+    $analyzer = new Analyzer();
+    $sentiment = $analyzer->getSentiment($comment);
 
-        return $this->redirectToRoute('app_commentairecinema_index', [], Response::HTTP_SEE_OTHER);
-    }
+    // Récupérer la catégorie de sentiment avec le score le plus élevé
+    $maxSentiment = array_keys($sentiment, max($sentiment))[0];
+
+    // Retourner la catégorie de sentiment
+    return $maxSentiment;
 }
+
+
+
+    
+        
+    }
+
+    
+
+
+
