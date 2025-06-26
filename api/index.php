fef89d6856e3d5f1eda319f2a5f4dc9f6421336c
aliammari1 <ammari.ali.0001@gmail.com>
2025-06-26 22:05:30 +0100

    Refactor index.php for improved error handling; disable mysqli and pdo_mysql extensions in php.ini; add Vercel command to composer.json

--- api/index.php
+++ api/index.php
@@ -1,3 +1,9 @@
 <?php
-
-require __DIR__ . '/../public/index.php';
+$file = __DIR__ . '/../public/' . $_SERVER['SCRIPT_NAME'];
+$path_parts = pathinfo($file);
+if(isset($path_parts['extension'])){
+    header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
+    echo "404 Not Found";
+    die;
+}
+return require __DIR__ . '/../public/index.php';
